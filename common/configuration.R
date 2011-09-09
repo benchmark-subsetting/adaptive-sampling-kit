34de45dace06c8a2117a41da5dd5b66939b1a3d3
Pablo Oliveira <pablo.oliveira@exascale-computing.eu>
2011-09-09 14:17:23 +0200

    Allow default values in R configuration reader.

--- common/configuration.R
+++ common/configuration.R
@@ -1,11 +1,15 @@
 require(rjson, quietly=T)
 configuration = fromJSON(paste(readLines(args[1]), collapse=""))
 
-conf <- function(key) {
+conf <- function(key, default=NULL) {
   dollarkey = gsub("\\.","$", key)
   v = eval(parse(text=paste("configuration$",dollarkey,sep="")))
   if (is.null(v)) {
-    stop(paste("Missing parameter ", key))
+    if (is.null(default)) {
+        stop(paste("Missing parameter ", key))
+    } else {
+        return(default)
+    }
   } else {
     return(v)
   }
