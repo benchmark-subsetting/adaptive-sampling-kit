benchmark-subsetting/adaptive-sampling-kit

commit e78309add1c0782c2f53bf4f8da2a1c0a577115b

live_configuration is now saved inside output directory

common/configuration.py
@@ -48,7 +48,8 @@ class Configuration():
             self.check_factors()
 
             # Write live configuration to disk
-            filename = "live_configuration.conf"
+            filename = os.path.join(self._conf["output_directory"], 
+                                    "live_configuration.conf")
             self._conf["configuration_file"] = filename
             f = open(filename, "w")
             json.dump(self._conf, f, indent=2)

commit 1cd366d599492b0f990148db61b76790b077d95c

Move output directory setup to configuration
* This fixes test regressions introduced by
e78309add1c0782c2f53bf4f8da2a1c0a577115b

The error happened because configuration tried to write the live_configuration
file into the outputdirectory which was not created at that stage. Now, we
ensure that when the live configuration is written, the output directory has
already been setup.

* Rename liveconfiguration file to .ask.conf

ask
@@ -150,7 +150,8 @@ def main():
 
     # Read Configuration
     C = Configuration(user_configuration=args.configuration,
-                      reload_live_configuration=False)
+                      reload_live_configuration=False,
+                      force_overwrite=args.force_overwrite)
 
     # Start Logger
     print "Logging to {0}".format(C("log.logfile"))
@@ -163,20 +164,6 @@ def main():
     fh.setLevel(eval("logging." + C("log.level")))
     L.addHandler(fh)
     L.debug("Logger started at level {0}".format(C("log.level")))
-
-    # Check that the output directory does not exist
-    if os.path.exists(C("output_directory")):
-        if args.force_overwrite:
-            L.warning("Removing existing output directory {0}"
-                      " since --force_overwrite was passed."
-                      .format(C("output_directory")))
-            shutil.rmtree(C("output_directory"))
-        else:
-            fatal("Output directory \"{0}\" already exists."
-                  " Use --force_overwrite option to discard it."
-                  .format(C("output_directory")))
-
-    os.makedirs(C("output_directory"))
 
     # Enter driver loop
     driver_loop()

common/configuration.py
@@ -2,6 +2,7 @@ import json
 import math
 import os
 import os.path
+import shutil
 
 from common.util import fatal
 
@@ -29,7 +30,8 @@ def check_executable(modulepath):
 class Configuration():
     def __init__(self,
                  user_configuration,
-                 reload_live_configuration=True):
+                 reload_live_configuration=True,
+                 force_overwrite=False):
 
         # If reloading a live configuration, there is
         # no need to check it for errors nor load the
@@ -48,6 +50,8 @@ class Configuration():
             # Overwrite default configuration with user's one
             Configuration.overwrite(self._conf, user_conf)
 
+            # Check and eventually create the output directory
+            self.check_outdir(force_overwrite)
             # Change and check modules paths
             self.check_modules()
 
@@ -55,13 +59,29 @@ class Configuration():
             self.check_factors()
 
             # Write live configuration to disk
-            filename = os.path.join(self._conf["output_directory"], 
-                                    "live_configuration.conf")
+            filename = os.path.join(self._conf["output_directory"],
+                                    ".ask.conf")
             self._conf["configuration_file"] = filename
             f = open(filename, "w")
             json.dump(self._conf, f, indent=2)
             f.write("\n")
             f.close()
+
+    def check_outdir(self, force_overwrite):
+        """ Ensure that the output directory can be used """
+
+        if os.path.exists(self("output_directory")):
+            if force_overwrite:
+                print("Removing existing output directory {0}"
+                        " since --force_overwrite was passed."
+                        .format(self("output_directory")))
+                shutil.rmtree(self("output_directory"))
+            else:
+                fatal("Output directory \"{0}\" already exists."
+                      " Use --force_overwrite option to discard it."
+                      .format(self("output_directory")))
+
+        os.makedirs(self("output_directory"))
 
     def check_factors(self):
         """ Check the factors configuration section """