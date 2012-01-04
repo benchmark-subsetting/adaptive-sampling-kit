16796ea78a187a111209ff9313e0612aa2c17f19
Pablo de Oliveira <pablo.oliveira@exascale-computing.eu>
2012-01-04 15:22:07 +0100

    Add a test for gauss2D example.

--- tests/test-0006-examples.py
+++ tests/test-0006-examples.py
@@ -15,6 +15,29 @@ class ExamplesTests(CommandLineT):
         assert "output/plot00000.png" in r.files_created,\
                "a plot is created"
 
+    def test_gauss2D(self):
+        """ Test that examples/gauss2D"""
+
+        # retrieve the configuration and data files
+        self.run("cp {0}/examples/gauss2D/test.data .".format(self.ask_path))
+        self.run("cp {0}/examples/gauss2D/source.R ."
+                .format(self.ask_path))
+
+        # change the default iterations values to make the test faster
+        import json
+        original_conf = json.loads(file("{0}/examples/gauss2D/experiment.conf"
+                                   .format(self.ask_path)).read())
+        original_conf["modules"]["control"]["params"]["points"] = 150
+        self.conf(original_conf)
+
+        r = self.run("ask test.conf")
+        assert "output/labelled.data" in r.files_created,\
+               "labelled.data is created"
+        assert "output/plot00002.png" in r.files_created,\
+               "a plot is created"
+
+
+
 
 
 
